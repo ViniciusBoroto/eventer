1a0bd91709afca3fb63577d8555a301dc983973f
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-06-01 19:40:25 -0300

    ref: removed atribbute on BaseString

--- API/Eventer/Contexts/ValueObject/BaseStringObject.cs
+++ API/Eventer/Contexts/ValueObject/BaseStringObject.cs
@@ -2,12 +2,9 @@
 {
     public class BaseStringObject
     {
-        public string Value { get; set; }
-
         public BaseStringObject(string value)
         {
             if (string.IsNullOrWhiteSpace(value)) throw new Exception("event's data must be filled");
-            Value = value;
         }
     }
 }
